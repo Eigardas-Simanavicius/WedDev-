bd20bc6aa0dc2a8c9562e5646d85612ca7c3a81e
Eigardas Simanavicius <eigardas24@gmail.com>
2026-01-29 12:27:44 +0000

    project setup

---
+++ demo/src/main/java/com/example/demo/MarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Marks this as the main class for spring boot
+public class MarketplaceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MarketplaceApplication.class, args);
+  } // starts the application
+
+}
